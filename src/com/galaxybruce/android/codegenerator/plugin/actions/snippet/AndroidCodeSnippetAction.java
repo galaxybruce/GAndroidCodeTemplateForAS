bf9d63f8ce5ae675415e5b21a48d2f4cce3a161f
bruce <brucezhanggood@gmail.com>
2021-07-07 16:12:29 +0800

    snippet支持本地文件

--- src/com/galaxybruce/android/codegenerator/plugin/actions/snippet/AndroidCodeSnippetAction.java
+++ src/com/galaxybruce/android/codegenerator/plugin/actions/snippet/AndroidCodeSnippetAction.java
@@ -2,9 +2,14 @@ package com.galaxybruce.android.codegenerator.plugin.actions.snippet;
 
 import com.galaxybruce.android.codegenerator.plugin.util.ClipboardHelper;
 import com.galaxybruce.android.codegenerator.plugin.util.FileUtils;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.components.panels.HorizontalBox;
@@ -13,10 +18,10 @@ import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -26,34 +31,53 @@ public class AndroidCodeSnippetAction extends AnAction {
 
     private static final Logger LOG = Logger.getInstance("AndroidCodeSnippetAction");
 
+    private static final String SNIPPET_PATH_KEY = "code_snippet_path";
+
     private static final int FRAME_WIDTH = 1000;
     private static final int FRAME_HEIGHT = 700;
+    private static final int TOP_HEIGHT = 60;
     private static final int MENU_HEIGHT = 100;
     private static final int TEMPLATE_LIST_WIDTH = 230;
 
 
+    private Project project;
 
-    protected JDialog jFrame;
+    private JDialog jFrame;
     private JTextArea codeArea;
+    private JPanel templateListPanel;
+    private JTextField pathField;
 
-    private ArrayList<TemplateInfo> templateInfoList;
-
-    private void initTemplateInfo() {
-        templateInfoList = new ArrayList<>();
-
-        templateInfoList.add(new TemplateInfo("snippet/RecyclerViewGridLayout.txt"));
-        templateInfoList.add(new TemplateInfo("snippet/LocalSingleton_Java.txt"));
-        templateInfoList.add(new TemplateInfo("snippet/LocalSingleton_kt.txt"));
-    }
+    private String snippetPath;
+    private ArrayList<TemplateInfo> templateInfoList = new ArrayList<>();
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         LOG.info("android snippet creates start... ");
-
-        initTemplateInfo();
+        project = event.getProject();
+        initTemplateInfo(PropertiesComponent.getInstance().getValue(SNIPPET_PATH_KEY));
         initView();
 
         LOG.info("android snippet creates end... ");
+    }
+
+    private void initTemplateInfo(String path) {
+        if(path == null || "".equals(path)) {
+            return;
+        }
+        File dir = new File(path);
+        if(!dir.exists()) {
+            return;
+        }
+        File[] list = dir.listFiles();
+        if(list == null || list.length == 0) {
+            return;
+        }
+
+        templateInfoList.clear();
+        for (File file : list) {
+            templateInfoList.add(new TemplateInfo(file.getName(), file.getAbsolutePath()));
+        }
+        this.snippetPath = path;
     }
 
     private void initView() {
@@ -61,22 +85,26 @@ public class AndroidCodeSnippetAction extends AnAction {
         jFrame = new JDialog();
         jFrame.setModal(true);
 
-
         // dialog中的面板设置布局样式
         Container container = jFrame.getContentPane();
         container.setLayout(new BorderLayout());
-
         // 主要区域
-        JPanel mainPanel = createMainPanel(container);
-        // 左边类型区域
-        JPanel templateTypePanel = createTemplateTypePanel(mainPanel);
-        initTemplateTypes(templateTypePanel);
+        JPanel mainPanel = createMainPanel();
+        container.add(mainPanel, BorderLayout.CENTER);
+
+        // 顶部选择模块文件存放路径
+        createFileChoosePanel(mainPanel);
+        this.pathField.setText(snippetPath);
+
+        // 左边模板列表区域
+        mainPanel.add(createTemplateListPanel(), BorderLayout.WEST);
+        updateTemplateList();
 
         // 代码显示区域
-        codeArea = createTemplateCodePanel(mainPanel);
+        mainPanel.add(createTemplateCodePanel(), BorderLayout.CENTER);
 
-        // 添加一个Panel，确定 取消按钮
-        createMenuPanel(container);
+        // 确定 取消按钮
+        container.add(createMenuPanel(), BorderLayout.SOUTH);
 
         // 显示窗口
         jFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
@@ -84,19 +112,82 @@ public class AndroidCodeSnippetAction extends AnAction {
         jFrame.setVisible(true);
     }
 
-    private void createMenuPanel(Container container) {
+    private JPanel createMainPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+//        panel.setBackground(Color.LIGHT_GRAY);
+        return panel;
+    }
+
+    private void createFileChoosePanel(JPanel mainPanel) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.setPreferredSize(new Dimension(FRAME_WIDTH, TOP_HEIGHT));
+        panel.setBorder(BorderFactory.createTitledBorder("Template path"));
+        mainPanel.add(panel, BorderLayout.NORTH);
+
+        JLabel titleLabel = new JLabel("Template file path: ");
+        titleLabel.setBorder(JBUI.Borders.emptyLeft(10));
+        titleLabel.setFont(new Font(null, Font.PLAIN, 13));
+//        titleLabel.setPreferredSize(new Dimension(TEMPLATE_LIST_WIDTH, 50));
+        panel.add(titleLabel);
+
+        // 用JPanel套起来，可以自适应布局变大变小
+        JPanel pathPanel = new JPanel();
+        pathPanel.setLayout(new BorderLayout());
+        pathPanel.setBorder(JBUI.Borders.empty(0, 10));
+        JTextField pathField = new JTextField();
+        pathField.setFont(new Font(null, Font.PLAIN, 13));
+        pathField.setPreferredSize(new Dimension(0, 35));
+        pathPanel.add(pathField);
+        panel.add(pathPanel);
+
+        JButton openChoose = new JButton("...");
+        openChoose.addActionListener(actionListener);
+        openChoose.setPreferredSize(new Dimension(35, 35));
+        panel.add(openChoose);
+
+        this.pathField = pathField;
+    }
+
+    private JPanel createTemplateListPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.setBorder(BorderFactory.createTitledBorder("Template list"));
+        panel.setPreferredSize(new Dimension(TEMPLATE_LIST_WIDTH, FRAME_HEIGHT - MENU_HEIGHT));
+
+        JPanel templateListPanel = new JPanel();
+        templateListPanel.setLayout(new BoxLayout(templateListPanel, BoxLayout.Y_AXIS));
+        JBScrollPane scrollPane = new JBScrollPane(templateListPanel);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        this.templateListPanel = templateListPanel;
+        return panel;
+    }
+
+    private Component createTemplateCodePanel() {
+        // template对应的代码Panel
+        JTextArea codeArea = new JTextArea();
+//        codeArea.setBorder(new LineBorder(JBColor.GRAY));
+
+        JBScrollPane panel = new JBScrollPane(codeArea);
+        panel.setBorder(BorderFactory.createTitledBorder("Code"));
+        this.codeArea = codeArea;
+        return panel;
+    }
+
+    private Component createMenuPanel() {
         VerticalBox verticalBox = new VerticalBox();
         verticalBox.add(Box.createVerticalGlue());
         verticalBox.setPreferredSize(new Dimension(FRAME_WIDTH, MENU_HEIGHT));
-        container.add(verticalBox, BorderLayout.SOUTH);
 
         JPanel panel = new JPanel();
         HorizontalBox horizontalBox = new HorizontalBox();
-        horizontalBox.add( Box.createHorizontalGlue() );
+        horizontalBox.add(Box.createHorizontalGlue());
         JButton cancel = new JButton("Cancel");
         cancel.setForeground(JBColor.RED);
         cancel.addActionListener(actionListener);
-        JButton ok = new JButton("Copy Code To Clipboard");
+        JButton ok = new JButton("Copy");
         ok.setForeground(JBColor.GREEN);
         ok.addActionListener(actionListener);
         horizontalBox.add(cancel);
@@ -108,92 +199,44 @@ public class AndroidCodeSnippetAction extends AnAction {
         panel.add(horizontalBox);
 
         verticalBox.add(panel);
+        return verticalBox;
     }
 
-    private JPanel createMainPanel(Container container) {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        panel.setBackground(Color.LIGHT_GRAY);
-        container.add(panel, BorderLayout.CENTER);
-        return panel;
-    }
-
-    private JPanel createTemplateTypePanel(JPanel mainPanel) {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        panel.setBackground(Color.DARK_GRAY);
-        panel.setPreferredSize(new Dimension(TEMPLATE_LIST_WIDTH, FRAME_HEIGHT - MENU_HEIGHT));
-        mainPanel.add(panel, BorderLayout.WEST);
-
-        JLabel titleLabel = new JLabel("Template Types");
-        titleLabel.setBorder(JBUI.Borders.emptyLeft(20));
-        titleLabel.setFont(new Font(null, Font.BOLD, 15));
-        titleLabel.setPreferredSize(new Dimension(TEMPLATE_LIST_WIDTH, 50));
-        panel.add(titleLabel, BorderLayout.NORTH);
-
-        JPanel templateTypePanel = new JPanel();
-        templateTypePanel.setLayout(new BoxLayout(templateTypePanel, BoxLayout.Y_AXIS));
-        JBScrollPane scrollPane = new JBScrollPane(templateTypePanel);
-        panel.add(scrollPane, BorderLayout.CENTER);
-
-        return templateTypePanel;
-    }
-
-    private JTextArea createTemplateCodePanel(JPanel mainPanel) {
-        // template对应的代码Panel
-        JTextArea codeArea = new JTextArea();
-        codeArea.setBorder(new LineBorder(JBColor.GRAY));
-
-        JBScrollPane panel = new JBScrollPane(codeArea);
-        mainPanel.add(panel, BorderLayout.CENTER);
-        return codeArea;
-    }
-
-    private void initTemplateTypes(JPanel templateTypePanel) {
+    private void updateTemplateList() {
+        templateListPanel.removeAll();
         ButtonGroup templateGroup = new ButtonGroup();
         for (TemplateInfo info : templateInfoList) {
-            String displayName = info.templateName;
-//            displayName = displayName.replaceAll("\\s+", "");
-            if(displayName.indexOf("/") > 0) {
-                displayName = displayName.substring(displayName.indexOf("/") + 1);
-            }
-            if(displayName.indexOf(".") > 0) {
-                displayName = displayName.substring(0, displayName.indexOf("."));
-            }
+            String displayName = info.name;
+//            if(displayName.indexOf("/") > 0) {
+//                displayName = displayName.substring(displayName.indexOf("/") + 1);
+//            }
+//            if(displayName.indexOf(".") > 0) {
+//                displayName = displayName.substring(0, displayName.indexOf("."));
+//            }
 
             JRadioButton radioButton = new JRadioButton(displayName, false);
-            radioButton.setActionCommand(info.templateName);
+            radioButton.setActionCommand(info.name);
             radioButton.addActionListener(radioActionListener);
-            templateTypePanel.add(radioButton);
             templateGroup.add(radioButton);
+
+            templateListPanel.add(radioButton);
         }
+        templateListPanel.updateUI();
     }
 
-    private ActionListener radioActionListener = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String cmd = e.getActionCommand();
-
-            for (TemplateInfo info : templateInfoList) {
-                if(info.templateName.equals(cmd)) {
-                    String content = FileUtils.readFile(this.getClass(), info.templateName);
-                    codeArea.setText(content);
-                    break;
-                }
-            }
+    private void chooseTemplatePath() {
+        FileChooserDescriptor descriptor = new FileChooserDescriptor(false, true,
+                false , false, false, false);
+        VirtualFile[] virtualFiles = FileChooser.chooseFiles(descriptor, project, null);
+        if(virtualFiles.length == 0) {
+            return;
         }
-    };
-
-    protected ActionListener actionListener = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getActionCommand().equals("Cancel")) {
-                dispose();
-            } else {
-                save();
-            }
-        }
-    };
+        String path = virtualFiles[0].getPath();
+        this.pathField.setText(path);
+        PropertiesComponent.getInstance().setValue(SNIPPET_PATH_KEY, path);
+        initTemplateInfo(path);
+        updateTemplateList();
+    }
 
     private void dispose() {
         jFrame.dispose();
@@ -204,11 +247,42 @@ public class AndroidCodeSnippetAction extends AnAction {
         dispose();
     }
 
-    private static class TemplateInfo {
-        String templateName;
+    private ActionListener radioActionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String cmd = e.getActionCommand();
+            for (TemplateInfo info : templateInfoList) {
+                if(info.name.equals(cmd)) {
+//                    String content = FileUtils.readFile(this.getClass(), info.templateName);
+                    String content = FileUtils.readToString(info.path);
+                    codeArea.setText(content);
+                    break;
+                }
+            }
+        }
+    };
 
-        public TemplateInfo(String templateName) {
-            this.templateName = templateName;
+    protected ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String cmd = e.getActionCommand();
+            if (cmd.equals("Cancel")) {
+                dispose();
+            } else if (cmd.equals("Copy")) {
+                save();
+            } else if (cmd.equals("...")) {
+                chooseTemplatePath();
+            }
+        }
+    };
+
+    private static class TemplateInfo {
+        String name;
+        String path;
+
+        public TemplateInfo(String name, String path) {
+            this.name = name;
+            this.path = path;
         }
     }
 }
--- src/com/galaxybruce/android/codegenerator/plugin/util/FileUtils.java
+++ src/com/galaxybruce/android/codegenerator/plugin/util/FileUtils.java
@@ -123,4 +123,26 @@ public class FileUtils {
         return "";
     }
 
+    public static String readToString(String fileName) {
+        String encoding = "UTF-8";
+        File file = new File(fileName);
+        Long fileLength = file.length();
+        byte[] fileContent = new byte[fileLength.intValue()];
+        try {
+            FileInputStream in = new FileInputStream(file);
+            in.read(fileContent);
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            return new String(fileContent, encoding);
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("The OS does not support " + encoding);
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
